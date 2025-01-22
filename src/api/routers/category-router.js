2403d1f65bf7433f4a39cf79db2265e8d2a234a6
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-01-23 00:16:41 +0500

    corrected swagger for categories

--- src/api/routers/category-router.js
+++ src/api/routers/category-router.js
@@ -17,6 +17,77 @@ const middleware = new Middleware(userRepository, roleRepository, itemRepository
 router.get(
     '/',
     (req, res, next) => middleware.isAuth(req, res, next),
+    /**
+     #swagger.tags = ['Categories']
+     #swagger.description = 'Retrieve categories with item count.'
+
+     #swagger.parameters['query'] = {
+     in: 'query',
+     description: 'A search query to filter categories by name or description.',
+     required: false,
+     schema: {
+     type: 'string'
+     }
+     }
+
+     #swagger.parameters['status'] = {
+     in: 'query',
+     description: 'The status of the items to filter by.',
+     required: false,
+     schema: {
+     type: 'string',
+     enum: ['approved', 'waiting', 'rejected'],
+     default: 'approved'
+     }
+     }
+
+     #swagger.parameters['dateFrom'] = {
+     in: 'query',
+     description: 'The start date to filter items.',
+     required: false,
+     schema: {
+     type: 'string',
+     format: 'date'
+     }
+     }
+
+     #swagger.parameters['dateTo'] = {
+     in: 'query',
+     description: 'The end date to filter items.',
+     required: false,
+     schema: {
+     type: 'string',
+     format: 'date'
+     }
+     }
+
+     #swagger.responses[200] = {
+     description: 'A list of categories with item counts.',
+     schema: {
+     categories: [
+     {
+     id: 'string',
+     name: 'string',
+     count: 0
+     }
+     ]
+     }
+     }
+
+     #swagger.responses[403] = {
+     description: 'Access denied if the user is not an admin and tries to access non-approved items.',
+     schema: {
+     detail: 'Access denied.'
+     }
+     }
+
+     #swagger.responses[500] = {
+     description: 'Internal server error.',
+     schema: {
+     detail: 'Internal server error.'
+     }
+     }
+     */
     (req, res) => categoryController.getCategories(req, res)
 );
 
@@ -24,6 +95,40 @@ router.post(
     '/add',
     (req, res, next) => middleware.isAuth(req, res, next),
     (req, res, next) => middleware.isAdmin(req, res, next),
+    /*
+    #swagger.tags = ['Categories']
+    #swagger.description = 'Add a new category.'
+
+    #swagger.parameters['body'] = {
+        in: 'body',
+        description: 'Data for creating a new category.',
+        required: true,
+        schema: {
+            $name: 'Electronics'
+        }
+    }
+
+    #swagger.responses[201] = {
+        description: 'Category created successfully.',
+        schema: {
+            detail: 'Category created successfully.'
+        }
+    }
+
+    #swagger.responses[400] = {
+        description: 'Validation error or missing required fields.',
+        schema: {
+            detail: 'Name is required.'
+        }
+    }
+
+    #swagger.responses[500] = {
+        description: 'Internal server error.',
+        schema: {
+            detail: 'Internal server error.'
+        }
+    }
+    */
     (req, res) => categoryController.addCategory(req, res)
 );
 
@@ -31,6 +136,47 @@ router.delete(
     '/delete/:id',
     (req, res, next) => middleware.isAuth(req, res, next),
     (req, res, next) => middleware.isAdmin(req, res, next),
+    /**
+     #swagger.tags = ['Categories']
+     #swagger.description = 'Delete a category by ID.'
+
+     #swagger.parameters['id'] = {
+     in: 'path',
+     description: 'The unique identifier of the category to be deleted.',
+     required: true,
+     schema: {
+     type: 'string'
+     }
+     }
+
+     #swagger.responses[204] = {
+     description: 'Category deleted successfully.',
+     schema: {
+     detail: 'Category deleted successfully.'
+     }
+     }
+
+     #swagger.responses[400] = {
+     description: 'Invalid category ID.',
+     schema: {
+     detail: 'Incorrect category ID.'
+     }
+     }
+
+     #swagger.responses[403] = {
+     description: 'Access denied if the user is not an admin.',
+     schema: {
+     detail: 'Access denied.'
+     }
+     }
+
+     #swagger.responses[500] = {
+     description: 'Internal server error.',
+     schema: {
+     detail: 'Internal server error.'
+     }
+     }
+     */
     (req, res) => categoryController.deleteCategory(req, res)
 );
 
@@ -38,7 +184,57 @@ router.put(
     '/update/:id',
     (req, res, next) => middleware.isAuth(req, res, next),
     (req, res, next) => middleware.isAdmin(req, res, next),
-    (req, res, next) => categoryController.updateCategory(req, res)
+    /**
+     #swagger.tags = ['Categories']
+     #swagger.description = 'Update a category by ID.'
+
+     #swagger.parameters['id'] = {
+     in: 'path',
+     description: 'The unique identifier of the category to be updated.',
+     required: true,
+     schema: {
+     type: 'string'
+     }
+     }
+
+     #swagger.parameters['body'] = {
+     in: 'body',
+     description: 'Data for updating the category.',
+     required: true,
+     schema: {
+     $name: 'Updated Category Name'
+     }
+     }
+
+     #swagger.responses[204] = {
+     description: 'Category updated successfully.',
+     schema: {
+     detail: 'Category updated successfully.'
+     }
+     }
+
+     #swagger.responses[400] = {
+     description: 'Validation error or missing required fields.',
+     schema: {
+     detail: 'Incorrect category ID.'
+     }
+     }
+
+     #swagger.responses[403] = {
+     description: 'Access denied if the user is not an admin.',
+     schema: {
+     detail: 'Access denied.'
+     }
+     }
+
+     #swagger.responses[500] = {
+     description: 'Internal server error.',
+     schema: {
+     detail: 'Internal server error.'
+     }
+     }
+     */
+    (req, res) => categoryController.updateCategory(req, res)
 )
 
 module.exports = router;
